c956c14dac1e989fd6211666da0195d07cb2ecd2
Clearsu <clearwater1219@gmail.com>
2024-12-07 20:19:11 +0100

    chore: remove unnecessary files

--- apps/HeadUnit/build/Qt_6_5_3_for_macOS-Debug/CMakeFiles/HeadUnit_qmlimportscan.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  ".qt_plugins/Qt6_QmlPlugins_Imports_HeadUnit.cmake"
-  "CMakeFiles/HeadUnit_qmlimportscan"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/HeadUnit_qmlimportscan.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
