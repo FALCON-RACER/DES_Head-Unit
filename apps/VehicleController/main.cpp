fe5a54cdb36890f5b3fb27fb746a45dfc41b8570
임원정 <oddish0513@gmail.com>
2025-01-16 19:39:00 +0100

    feat: driving piracer test code #8

--- apps/VehicleController/main.cpp
+++ apps/VehicleController/main.cpp
@@ -1,9 +1,41 @@
-#include <iostream>
+#include "PiRacer/PiRacer.hpp"
+#include "Gamepad/ShanwanGamepad.hpp"
 
-using namespace std;
+/**
+ * A : DRIVE
+ * B : NEUTRAL
+ * X : REVERSE
+ * Y : PARKING
+ */
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    if (gpioInitialise() < 0)
+    {
+        std::cerr << "pigpio initialization failed" << std::endl;
+        return 1;
+    }
+    atexit(gpioTerminate);
+
+    PiRacer racer;
+    ShanWanGamepad gamepad;
+
+    while (true)
+    {
+        ShanWanGamepadInput input = gamepad.read_data();
+
+        std::cout << input.button_a << std::endl;
+
+        float steering = input.analog_stick_left.x;
+        float throttle = input.analog_stick_right.y * 0.5; // limit throtle to 50%
+
+        std::cout << "Throttle: " << throttle
+                  << ", Steering: " << steering
+                  << std::endl;
+
+        racer.setSteeringPercent(steering);
+        racer.setThrottlePercent(throttle);
+    }
+
     return 0;
 }
