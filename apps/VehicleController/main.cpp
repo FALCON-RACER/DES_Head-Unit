73b6030d986092554cd34a02c51f60b5ec44de58
임원정 <oddish0513@gmail.com>
2025-01-17 01:01:18 +0100

    fix: edit for drive #8

--- apps/VehicleController/main.cpp
+++ apps/VehicleController/main.cpp
@@ -28,6 +28,8 @@ int main()
 
         float steering = input.analog_stick_left.x;
         float throttle = input.analog_stick_right.y * 0.5; // limit throtle to 50%
+        float steering = input.analog_stick_left.x * -1;
+        float throttle = input.analog_stick_right.x * 0.7; // limit throtle to 50%
 
         std::cout << "Throttle: " << throttle
                   << ", Steering: " << steering
