38adae80650402b57ea5c2f7f3d22c35a8af5db0
임원정 <oddish0513@gmail.com>
2025-01-17 02:50:00 +0100

    feat: add gear feature #8

--- apps/VehicleController/main.cpp
+++ apps/VehicleController/main.cpp
@@ -1,6 +1,5 @@
 #include "PiRacer/PiRacer.hpp"
 #include "Gamepad/ShanwanGamepad.hpp"
-#include <cstdlib>
 
 /**
  * A : DRIVE
@@ -9,24 +8,50 @@
  * Y : PARKING
  */
 
-int main()
-{
-    if (gpioInitialise() < 0)
-    {
+enum Gear {
+    PARKING,
+    REVERSE,
+    NEUTRAL,
+    DRIVE
+};
+
+int main() {
+    atexit(gpioTerminate);
+    
+	if (gpioInitialise() < 0) {
         std::cerr << "pigpio initialization failed" << std::endl;
         return 1;
     }
-    atexit(gpioTerminate);
 
     PiRacer racer;
     ShanWanGamepad gamepad;
+    Gear gear = PARKING;
 
-    while (true)
-    {
+    float steering = 0;
+    float throttle = 0;
+
+    while (true) {
         ShanWanGamepadInput input = gamepad.read_data();
 
-        float steering = input.analog_stick_left.x;
-        float throttle = input.analog_stick_right.x * 0.7;
+        steering = 0;
+        throttle = 0;
+
+        if (input.button_a)
+            gear = DRIVE;
+        else if (input.button_b)
+            gear = NEUTRAL;
+        else if (input.button_x)
+            gear = REVERSE;
+        else if (input.button_y)
+            gear = PARKING;
+        else if (gear == PARKING || gear == NEUTRAL)
+            continue;
+
+        steering = input.analog_stick_left.x;
+        if (input.analog_stick_right.x <= 0)
+            throttle = input.analog_stick_right.x * -0.7;
+        if (gear == DRIVE)
+            throttle *= -1;
 
         racer.setSteeringPercent(steering);
         racer.setThrottlePercent(throttle);
@@ -34,3 +59,4 @@ int main()
 
     return 0;
 }
+
