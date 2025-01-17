ef3ff935da39c968440d80e1d68ee43cb09df159
임원정 <oddish0513@gmail.com>
2025-01-17 01:53:50 +0100

    chore: remove the comments #8

--- apps/VehicleController/main.cpp
+++ apps/VehicleController/main.cpp
@@ -25,16 +25,11 @@ int main()
     {
         ShanWanGamepadInput input = gamepad.read_data();
 
-        std::cout << input.button_a << std::endl;
-
         float steering = input.analog_stick_left.x;
         float throttle = input.analog_stick_right.y * 0.5; // limit throtle to 50%
         float steering = input.analog_stick_left.x * -1;
         float throttle = input.analog_stick_right.x * 0.7; // limit throtle to 50%
 
-        std::cout << "Throttle: " << throttle
-                  << ", Steering: " << steering
-                  << std::endl;
 
         racer.setSteeringPercent(steering);
         racer.setThrottlePercent(throttle);
