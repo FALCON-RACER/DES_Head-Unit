8a67021b1c591daac8a2d36b35ae3028fafaf94c
임원정 <oddish0513@gmail.com>
2025-02-20 14:57:16 +0100

    feat: send and receive vSOME/IP data in vehicle controller

--- apps/VehicleController/main.cpp
+++ apps/VehicleController/main.cpp
@@ -1,5 +1,6 @@
 #include "PiRacer/PiRacer.hpp"
 #include "Gamepad/ShanwanGamepad.hpp"
+#include "../ServiceManager/src/server.hpp"
 
 /**
  * A : DRIVE
@@ -15,6 +16,60 @@ enum Gear {
     DRIVE
 };
 
+Gear gear = PARKING;
+std::shared_ptr<vsomeip::application> app;
+std::mutex mutex;
+std::condition_variable condition;
+
+// sending the actual data (will be gear data) to server.
+void send_gear_data(int data) {
+    std::unique_lock<std::mutex> its_lock(mutex);
+    condition.wait(its_lock);
+
+    std::shared_ptr<vsomeip::message> request;
+    request = vsomeip::runtime::get()->create_request();
+    request->set_service(VEHICLE_SERVICE_ID);
+    request->set_instance(GEAR_INSTANCE_ID);
+    request->set_method(JOY_GEAR_SET_MID);
+
+    std::shared_ptr<vsomeip::payload> its_payload = vsomeip::runtime::get()->create_payload();
+    std::vector<vsomeip::byte_t> its_payload_data(
+        reinterpret_cast<vsomeip::byte_t *>(&data),
+        reinterpret_cast<vsomeip::byte_t *>(&data) + sizeof(int));
+    its_payload->set_data(its_payload_data);
+    request->set_payload(its_payload);
+    app->send(request);
+    std::cout << "CLIENT : DATA SENDED" << std::endl;
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+}
+
+// When response come, print the payload from server.
+void on_message(const std::shared_ptr<vsomeip::message> &_response) {
+    std::shared_ptr<vsomeip::payload> payload = _response->get_payload();
+    int received_value = 0;
+
+    if (payload->get_length() >= sizeof(int)) {
+        received_value = *reinterpret_cast<const int *>(payload->get_data());
+        std::cout << "SERVER: Received int: " << received_value << std::endl;
+    }
+    else {
+        std::cerr << "SERVER: Invalid payload size!" << std::endl;
+        return;
+    }
+}
+
+void initVSOMEIP() {
+    app = vsomeip::runtime::get()->create_application("gear");
+    app->init();
+    app->register_availability_handler(VEHICLE_SERVICE_ID, GEAR_INSTANCE_ID, on_availability);
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+    app->request_service(VEHICLE_SERVICE_ID, GEAR_INSTANCE_ID);
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+    app->register_message_handler(VEHICLE_SERVICE_ID, GEAR_INSTANCE_ID, JOY_GEAR_SET_MID, on_message);
+    std::thread sender(run);
+    app->start();
+}
+
 int main() {
     atexit(gpioTerminate);
 
@@ -23,9 +78,9 @@ int main() {
         return 1;
     }
 
+    initVSOMEIP();
     PiRacer racer;
     ShanWanGamepad gamepad;
-    Gear gear = PARKING;
 
     float steering = 0;
     float throttle = 0;
@@ -42,6 +97,12 @@ int main() {
                 gear = REVERSE;
             else if (input.button_y)
                 gear = PARKING;
+
+            if (gear == PARKING || gear == NEUTRAL) {
+                racer.setSteeringPercent(0);
+                racer.setThrottlePercent(0);
+            }
+            send_gear_data(gear);
         }
         else {
             if (gear == PARKING || gear == NEUTRAL)
@@ -60,7 +121,4 @@ int main() {
             racer.setThrottlePercent(throttle);
         }
     }
-
-    return 0;
 }
-
