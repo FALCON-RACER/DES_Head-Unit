c72ebe1eb910561a32de6e371be6727f25c9d0c9
SihunLee-1016 <sspkr1782@gmail.com>
2025-02-24 17:52:19 +0100

    qt comiple setup

--- apps/HeadUnit/clients/speed_client/speed_client.hpp
+++ apps/HeadUnit/clients/speed_client/speed_client.hpp
@@ -11,12 +11,11 @@ public:
     bool init();
     void start();
     void stop();
-
+    float speedValue;
 signals:
     void speedReceived(int speed);
 
 private:
-    float speedValue;
     void on_state(vsomeip::state_type_e _state);
     void on_availability(vsomeip::service_t _service, vsomeip::instance_t _instance, bool _is_available);
     void on_message(const std::shared_ptr<vsomeip::message>& _response);
