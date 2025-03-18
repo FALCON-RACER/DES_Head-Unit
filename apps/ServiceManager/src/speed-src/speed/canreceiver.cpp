3a149eb0ec64138525764f5073c2f33966ba9573
SihunLee-1016 <sspkr1782@gmail.com>
2025-03-18 15:12:30 +0100

    11

--- apps/ServiceManager/src/speed-src/speed/canreceiver.cpp
+++ apps/ServiceManager/src/speed-src/speed/canreceiver.cpp
@@ -86,7 +86,7 @@ float CANReceiver::calculateSpeed(const struct can_frame* frame) {
     // Interpret speed as a signed 16-bit integer
     float speed=0;
     // memcpy (&speed, frame->data, sizeof(float));
-    memcpy (&speed, frame.data, sizeof(float));
+    memcpy (&speed, frame->data, sizeof(float));
     // std::cout << "memcpy value : " <<speed << std::endl;
 
     // std::cout << " 0 : "<<static_cast<int>(frame->data[0]) << std::endl;
